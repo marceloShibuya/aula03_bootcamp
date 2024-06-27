915835cd662dbc7697b9aca68676a2f8d4084b28
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-06-26 23:25:43 -0300

    adicionando exemplo de FOR com uma lista

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 
-numero = int(input("Digite um número inteiro: "))
+#EXEMPLO DE IF
 
-if isinstance(numero,int):
-    if numero < 0:
-        print("O número é menor que 0")
-    elif numero >= 1 and numero <= 10:
-        print("O número é entre 1 a 10")
-    else:
-        print("O número é maior do que 10")
-else:
-    exit
+# numero = int(input("Digite um número inteiro: "))
+
+# if isinstance(numero,int):
+#     if numero < 0:
+#         print("O número é menor que 0")
+#     elif numero >= 1 and numero <= 10:
+#         print("O número é entre 1 a 10")
+#     else:
+#         print("O número é maior do que 10")
+# else:
+#     exit
+
+#EXEMPLO DE FOR
+# 1
+# for i in range(1,5):
+#     print(i)
+
+#2
+lista_de_alunos = ["Marcelo","Ricardo","Denise"]
+
+for i in lista_de_alunos:
+    print(i)
+
 
